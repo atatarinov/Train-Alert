029624e98e5af12e089a061b6534b08a87a0729e
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2017-11-21 17:26:23 -0500

    put app.js back to the front

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
-import Home from './Home';
-import StatusView from './StatusView';
-import NotificationView from './NotificationView';
+import Home from './components/Home';
+import StatusView from './components/StatusView';
+import NotificationView from './components/NotificationView';
 
 const Views = StackNavigator({
   Home: { screen: Home },
